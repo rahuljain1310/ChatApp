7a709434a726205b2b4842f7e968813d7a42dc55
Rahul Jain <you@example.com>
2019-03-10 15:54:44 +0530

    Streaming through webcam

--- server/app.py
+++ server/app.py
@@ -1,17 +1,16 @@
 import sys
-from flask import Flask,render_template
+import cv2
+from flask import Flask,render_template,Response
 from flask_socketio import SocketIO
 from flaskext.mysql import MySQL
-
+from camera import Camera
 
 PORT = 5000
 ROOT_URL = 'http://localhost:{}'.format(PORT)
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
 app.config['PORT']=5000
-# =============================================================================
 # app.config['DEBUG']=True
-# =============================================================================
 mysql = MySQL()
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = ''
@@ -27,6 +26,42 @@ socketio = SocketIO(app)
 def sessions():
     return render_template('index.html')
 
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+def get_frame():
+    camera_port=0
+    # ramp_frames=100
+    camera=cv2.VideoCapture(camera_port)
+    # i=1
+    while True:
+        retval, im = camera.read()
+        imgencode=cv2.imencode('.jpg',im)[1]
+        stringData=imgencode.tostring()
+        yield (b'--frame\r\n'
+            b'Content-Type: text/plain\r\n\r\n'+stringData+b'\r\n')
+        # i+=1
+    camera.release()
+    del(camera)
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(get_frame(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/video_feed/<int:usr>')
+def video_feed_usr(usr):
+    if(usr==1):
+        return Response(get_frame(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    if(usr==2):
+        return Response(gen(Camera()),mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/agora')
+def agora():
+    return render_template("agora.html")
+
 def messageReceived(methods=['GET', 'POST']):
     print('message was received!!!')
 
@@ -38,21 +73,18 @@ def handle_my_custom_event(json, methods=['GET', 'POST']):
     conn.commit()
     socketio.emit('my response', json, callback=messageReceived)
 
-# =============================================================================
-# class Example(QWidget):
-#     def __init__(self):
-#         super().__init__()
-#         self.initUI()    
-#     def initUI(self):               
-#         qbtn = QPushButton('Quit', self)
-#         qbtn.clicked.connect(QApplication.instance().quit)
-#         qbtn.resize(qbtn.sizeHint())
-#         qbtn.move(50, 50)       
-#         self.setGeometry(300, 300, 250, 150)
-#         self.setWindowTitle('Quit button')    
-#         self.show()
-#     
-# =============================================================================
+@socketio.on("connect user")
+def handke_user_connect(json, methods=['GET', 'POST']):
+    log = 'received message: ' + str(json)
+    print(log)
+    cursor.execute('INSERT INTO log VALUES (%s)',log)
+    conn.commit()
+    Response = json
+    # if (json.user_name==1):
+    #     response = Response(get_frame(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    # if (json.user_name==2):
+    #     response = Response(gen(Camera()),mimetype='multipart/x-mixed-replace; boundary=frame')
+    socketio.emit('my video response', Response, callback=messageReceived)
 
 # =============================================================================
 # class FlaskThread(QThread):
@@ -63,9 +95,7 @@ def handle_my_custom_event(json, methods=['GET', 'POST']):
 #         self.wait()
 #     def run(self):
 #         socketio.run(self.application)
-# =============================================================================
-    
-# =============================================================================
+#
 # def provide_GUI_for(application):
 #     qtapp = QApplication(sys.argv)
 #     webapp = FlaskThread(application)
@@ -79,15 +109,4 @@ def handle_my_custom_event(json, methods=['GET', 'POST']):
         
 if __name__ == '__main__':    
     socketio.run(app)
-# =============================================================================
-#     webapp = FlaskThread(app)
-#     webapp.start()
-#     qtapp.aboutToQuit.connect(webapp.terminate)
-# =============================================================================
-# =============================================================================
-#     webview = QWebEngineView()
-#     webview.load(QUrl(ROOT_URL))
-#     webview.show()
-#     sys.exit(qtapp.exec_())
-# 
-# =============================================================================
+    # app.run()
