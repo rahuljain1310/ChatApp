2b59919fcdae56ce22eb8c9473d116d3c0021220
Rahul Jain <you@example.com>
2019-03-14 01:47:11 +0530

    Video Streaming through Python Socket Added

--- server/app.py
+++ server/app.py
@@ -4,6 +4,8 @@ from flask import Flask,render_template,Response
 from flask_socketio import SocketIO
 from flaskext.mysql import MySQL
 from camera import Camera
+import threading
+import pickle
 
 PORT = 5000
 ROOT_URL = 'http://localhost:{}'.format(PORT)
@@ -19,48 +21,85 @@ app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 conn = mysql.connect()
 cursor =conn.cursor()
-
 socketio = SocketIO(app)
+
+im = 0
 
 @app.route('/')
 def sessions():
     return render_template('index.html')
 
-def gen(camera):
-    while True:
-        frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+# @app.route('/agora')
+# def agora():
+#     return render_template("agora.html")
+
+@app.route('/stream')
+def stream():
+    return render_template("stream.html")
+
+# def gen(camera):
+#     while True:
+#         frame = camera.get_frame()
+#         yield (b'--frame\r\n'
+#                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 def get_frame():
     camera_port=0
     # ramp_frames=100
     camera=cv2.VideoCapture(camera_port)
-    # i=1
+    i=1
     while True:
         retval, im = camera.read()
         imgencode=cv2.imencode('.jpg',im)[1]
+        data = pickle.dumps(imgencode, 0)
+        size = len(data)
         stringData=imgencode.tostring()
+        # x = (b'--frame\r\n'
+        #     b'Content-Type: text/plain\r\n\r\n'+stringData+b'\r\n')
+        # json = {"strdata":x}
+        # if(i%100==0):
+        #     socketio.emit('fr', json, callback=messageReceived)
         yield (b'--frame\r\n'
             b'Content-Type: text/plain\r\n\r\n'+stringData+b'\r\n')
-        # i+=1
+        # x = (b'--frame\r\n'
+        #     b'Content-Type: text/plain\r\n\r\n'+stringData+b'\r\n')
     camera.release()
     del(camera)
+
+
+# def copy_frame():
+#     while True:
+#         imgencode=cv2.imencode('.jpg',im)[1]
+#         stringData=imgencode.tostring()
+#         yield (b'--frame\r\n'
+#             b'Content-Type: text/plain\r\n\r\n'+stringData+b'\r\n')
+
+# def send_frame():
+#     camera=cv2.VideoCapture(0)
+#     while(True):
+#         retval,im = camera.read()
+#         imgencode = cv2.imencode('.jpg',im)[1]
+#         stringData=imgencode.tostring()
+#         x = (b'--frame\r\n'
+#             b'Content-Type: text/plain\r\n\r\n'+stringData+b'\r\n')
+#         socketio.emit('frame response',{"frame": x})
+#         # yield (b'--frame\r\n'
+#         #     b'Content-Type: text/plain\r\n\r\n'+stringData+b'\r\n')
+#     camera.release()
+#     del (camera)
 
 @app.route('/video_feed')
 def video_feed():
     return Response(get_frame(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-@app.route('/video_feed/<int:usr>')
-def video_feed_usr(usr):
-    if(usr==1):
-        return Response(get_frame(), mimetype='multipart/x-mixed-replace; boundary=frame')
-    if(usr==2):
-        return Response(gen(Camera()),mimetype='multipart/x-mixed-replace; boundary=frame')
+# @app.route('/video_feed/<int:usr>')
+# def video_feed_usr(usr):
+#     return Response(copy_frame(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    # if(usr==1):
+        # return Response(get_frame(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    # if(usr==2):
+        # return Response(gen(Camera()),mimetype='multipart/x-mixed-replace; boundary=frame')
 
-@app.route('/agora')
-def agora():
-    return render_template("agora.html")
 
 def messageReceived(methods=['GET', 'POST']):
     print('message was received!!!')
@@ -73,18 +112,14 @@ def handle_my_custom_event(json, methods=['GET', 'POST']):
     conn.commit()
     socketio.emit('my response', json, callback=messageReceived)
 
-@socketio.on("connect user")
-def handke_user_connect(json, methods=['GET', 'POST']):
-    log = 'received message: ' + str(json)
-    print(log)
-    cursor.execute('INSERT INTO log VALUES (%s)',log)
-    conn.commit()
-    Response = json
-    # if (json.user_name==1):
-    #     response = Response(get_frame(), mimetype='multipart/x-mixed-replace; boundary=frame')
-    # if (json.user_name==2):
-    #     response = Response(gen(Camera()),mimetype='multipart/x-mixed-replace; boundary=frame')
-    socketio.emit('my video response', Response, callback=messageReceived)
+# @socketio.on("start recording")
+# def handke_user_connect(json, methods=['GET', 'POST']):
+#     log = 'Start Recording: ' + str(json)
+#     print(log)
+#     cursor.execute('INSERT INTO log VALUES (%s)',log)
+#     conn.commit()
+#     thr = threading.Thread(target=get_frame, args=(), kwargs={})
+#     thr.start()
 
 # =============================================================================
 # class FlaskThread(QThread):
@@ -95,16 +130,6 @@ def handke_user_connect(json, methods=['GET', 'POST']):
 #         self.wait()
 #     def run(self):
 #         socketio.run(self.application)
-#
-# def provide_GUI_for(application):
-#     qtapp = QApplication(sys.argv)
-#     webapp = FlaskThread(application)
-#     webapp.start()
-#     qtapp.aboutToQuit.connect(webapp.terminate)
-#     webview = QWebEngineView()
-#     webview.load(QUrl(ROOT_URL))
-#     webview.show()
-#     return qtapp.exec_()
 # =============================================================================
         
 if __name__ == '__main__':    
--- server/camera.py
+++ server/camera.py
@@ -1,8 +1,10 @@
 from time import time
 
 class Camera(object):
+    last_access = 0    # static variable in class
     def __init__(self):
         self.frames = [open(f + '.jpg', 'rb').read() for f in ['1', '2', '3']]
 
     def get_frame(self):
+        # Camera.last_access = time.time()
         return self.frames[int(time()) % 3]
